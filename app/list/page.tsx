5c42ecb81014ade226b2d7f21d93f252a244a971
Raman Prasad <ramanprasad.0203@gmail.com>
2025-08-05 13:20:58 +0530

    feat: add ContactPage and ListPage components with search functionality

---
+++ app/list/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React from 'react'
+import { dummyData } from '@/dummyData.mjs'
+import { FixedSizeList as List } from 'react-window'
+import SearchInput from '@/components/search'
+function ListPage() {
+  if (!dummyData || dummyData.length === 0) {
+    return <h1 className="text-center mt-8">No data available</h1>
+  }
+  return (
+    <div className="bg-first flex flex-col items-center">
+      <SearchInput />
+      <List
+        height={700}
+        itemCount={dummyData.length}
+        itemSize={76 + 8}
+        width={'95%'}
+        itemData={dummyData}
+        className="mb-2 p-4 bg-white rounded shadow"
+      >
+        {({ index, style }) => {
+          const item = dummyData[index]
+
+          return (
+            <div
+              style={style}
+              key={item.id}
+              className="p-4 border-y-8  border-first bg-third flex flex-row justify-between items-center"
+            >
+              <div className="flex flex-col justify-center items-start  gap-2">
+                <h2 className="text-lg font-semibold">{item.full_name}</h2>
+                <p className="text-sm text-gray-600">{item.mobile}</p>
+              </div>
+              <p className="text-xl text-gray-600">{item.amount}</p>
+            </div>
+          )
+        }}
+      </List>
+    </div>
+  )
+}
+
+export default ListPage
