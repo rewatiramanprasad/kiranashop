7e6a8aabaf91a0dbdb62eb64dabfd895e2eefd49
Raman Prasad <ramanprasad.0203@gmail.com>
2025-08-07 00:52:13 +0530

    feat: update dummy data with new entries and remarks for Pizza Bread

--- dummyData.mjs
+++ dummyData.mjs
@@ -1,251 +1,259 @@
-export const dummyData = [{
-  "id": 1,
-  "full_name": "Constantine Clandillon",
-  "mobile": "636 539 4281",
-  "amount": "Rs.3000"
-}, {
-  "id": 2,
-  "full_name": "Margeaux Tampion",
-  "mobile": "495 435 2990",
-  "amount": "Rs.1000"
-}, {
-  "id": 3,
-  "full_name": "Valeda Twiggins",
-  "mobile": "624 669 8547",
-  "amount": "Rs.6000"
-}, {
-  "id": 4,
-  "full_name": "Charlton Chapelle",
-  "mobile": "335 779 5698",
-  "amount": "Rs.7000"
-}, {
-  "id": 5,
-  "full_name": "Domenic Hutchison",
-  "mobile": "288 263 5760",
-  "amount": "Rs.7000"
-}, {
-  "id": 6,
-  "full_name": "Em Ruslin",
-  "mobile": "460 313 7541",
-  "amount": "Rs.2000"
-}, {
-  "id": 7,
-  "full_name": "Torrey Purselow",
-  "mobile": "338 519 0043",
-  "amount": "Rs.2000"
-}, {
+export const dummyData = [ {
   "id": 8,
   "full_name": "Adoree Arbuckel",
   "mobile": "565 812 8642",
-  "amount": "Rs.7000"
+  "amount": "7000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 9,
   "full_name": "Xavier Delves",
   "mobile": "861 170 7194",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 10,
   "full_name": "Livy Petris",
   "mobile": "174 455 9987",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 11,
   "full_name": "Eunice Mongan",
   "mobile": "420 559 4514",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 12,
   "full_name": "Hayes Nelligan",
   "mobile": "490 893 2668",
-  "amount": ""
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 13,
   "full_name": "Bill Vigurs",
   "mobile": "535 441 1848",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 14,
   "full_name": "Whitaker Amiss",
   "mobile": "650 948 6962",
-  "amount": "Rs.5000"
+  "amount": "5000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 15,
   "full_name": "Rustie Cave",
   "mobile": "835 151 4480",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 16,
   "full_name": "Aurelie Jacquemard",
   "mobile": "938 262 1890",
-  "amount": "Rs.5000"
+  "amount": "5000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 17,
   "full_name": "Joyous Simpkin",
   "mobile": "101 361 1950",
-  "amount": "Rs.5000"
+  "amount": "5000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 18,
   "full_name": "Meredithe Lyddon",
   "mobile": "475 200 6521",
-  "amount": "Rs.3000"
+  "amount": "3000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 19,
   "full_name": "Batsheva Capitano",
   "mobile": "181 538 0578",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 20,
   "full_name": "Christiana Knappen",
   "mobile": "648 205 5955",
-  "amount": "Rs.7000"
+  "amount": "7000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 21,
   "full_name": "Dermot Filippazzo",
   "mobile": "197 989 1932",
-  "amount": "Rs.7000"
+  "amount": "7000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 22,
   "full_name": "Tawsha Bennitt",
   "mobile": "848 817 3312",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 23,
   "full_name": "Glenden Halls",
   "mobile": "341 410 6131",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 24,
   "full_name": "Wilmer Shah",
   "mobile": "289 611 5232",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 25,
   "full_name": "Rasla Wadley",
   "mobile": "853 241 7996",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 26,
   "full_name": "Mitzi Cuckson",
   "mobile": "247 395 5630",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 27,
   "full_name": "Huntley Bockler",
   "mobile": "525 284 3739",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 28,
   "full_name": "Brent Couper",
   "mobile": "660 833 2608",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 29,
   "full_name": "Jerrylee Feighry",
   "mobile": "617 523 7688",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 30,
   "full_name": "Hertha Di Angelo",
   "mobile": "316 845 5119",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 31,
   "full_name": "Charita Kemitt",
   "mobile": "836 305 3420",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 32,
   "full_name": "Vasily Dockery",
   "mobile": "438 491 5703",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 33,
   "full_name": "Renell Triggs",
   "mobile": "866 794 2212",
-  "amount": "Rs.3000"
+  "amount": "3000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 34,
   "full_name": "Travis Korous",
   "mobile": "270 527 3216",
-  "amount": "Rs.5000"
+  "amount": "5000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 35,
   "full_name": "Constantin Mazzilli",
   "mobile": "511 779 3239",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 36,
   "full_name": "Hilary Wedon",
   "mobile": "749 422 8674",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 37,
   "full_name": "Arlen Hasker",
   "mobile": "116 164 4251",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 38,
   "full_name": "Cazzie Tomney",
   "mobile": "534 603 0669",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 39,
   "full_name": "Cate Arbor",
   "mobile": "714 361 2010",
-  "amount": "Rs.7000"
+  "amount": "7000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 40,
   "full_name": "Lynnette Murrthum",
   "mobile": "721 813 3826",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 41,
   "full_name": "Chariot Phythien",
   "mobile": "779 295 2885",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 42,
   "full_name": "Lainey Weeks",
   "mobile": "377 751 1726",
-  "amount": "Rs.7000"
+  "amount": "7000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 43,
   "full_name": "Alex Coweuppe",
   "mobile": "115 300 3550",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 44,
   "full_name": "Francisca Southouse",
   "mobile": "138 654 4788",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 45,
   "full_name": "Diann Petheridge",
   "mobile": "832 292 4317",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 46,
   "full_name": "Eugen Garretts",
   "mobile": "773 594 0633",
-  "amount": "Rs.4000"
+  "amount": "4000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 47,
   "full_name": "Jedediah Ginnety",
   "mobile": "188 415 7690",
-  "amount": "Rs.2000"
+  "amount": "2000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 48,
   "full_name": "Katya Frudd",
   "mobile": "143 783 7149",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 49,
   "full_name": "Corabelle Kitcherside",
   "mobile": "703 310 8351",
-  "amount": "Rs.6000"
+  "amount": "6000",
+  "remarks": "Pizza Bread(20 pcs)"
 }, {
   "id": 50,
   "full_name": "Kermit Ream",
   "mobile": "253 971 9189",
-  "amount": "Rs.1000"
+  "amount": "1000",
+  "remarks": "Pizza Bread(20 pcs)"
 }]
