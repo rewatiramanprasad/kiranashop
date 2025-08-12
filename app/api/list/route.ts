b1f1a719d3e4c83e0e03e08efda41f1f7c89eab8
Raman Prasad <ramanprasad.0203@gmail.com>
2025-08-13 02:26:25 +0530

    fix: update API call to use getDuesList and improve error handling in ListPage component

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,11 +1,11 @@
-import { getDues } from "@/server/duesModel";
+import { getDuesList } from "@/server/duesModel";
 import { NextResponse } from "next/server";
 import {seeding} from '@/server/seeding'
 
 
 export async function GET() { 
 
-    const data = await getDues();
+    const data = await getDuesList();
     // await seeding()
 
     return NextResponse.json({data:data,success: true, message: `${data.length} rows fetched successfully`},{status: 200});
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,18 +2,31 @@ import React from 'react'
 import { dummyData } from '@/dummyData.mjs'
 import Container from '@/components/container'
 import SearchAndList from '@/components/List'
-async function ListPage() {
-  const res = await fetch('http://localhost:3000/api/list')
-  const data = await res.json()
-  console.log(data)
 
-  return (
-    // <div className="bg-first flex flex-col items-center">
-    // </div>
-    <Container>
-      <SearchAndList data={data.data} />
-    </Container>
-  )
+async function ListPage() {
+  try {
+    const res = await fetch('http://localhost:3000/api/list')
+    const data = await res.json()
+    if (data.success === false) {
+      throw new Error(data.message || 'Failed to fetch list data')
+    }
+
+    return (
+      <Container>
+        <SearchAndList data={data.data} />
+      </Container>
+    )
+  } catch (error) {
+    console.error('Error fetching list data:', error)
+
+    return (
+      <h1 className=" text-2xl text-center text-red-500">
+        {typeof error === 'object' && error !== null && 'message' in error
+          ? (error as { message?: string }).message
+          : 'An error occurred'}
+      </h1>
+    )
+  }
 }
 
 export default ListPage
