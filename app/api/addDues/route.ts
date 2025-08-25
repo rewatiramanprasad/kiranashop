7d3b384bee56953dc02d8974fcd9ae14fb65ca7c
Raman Prasad <ramanprasad.0203@gmail.com>
2025-08-25 13:52:17 +0530

    refactor: unnecessary comment

--- app/api/addDues/route.ts
+++ app/api/addDues/route.ts
@@ -2,9 +2,8 @@ import { createDues, Dues } from '@/server/duesModel'
 export async function POST(request: Request) {
   try {
     const body = await request.json()
-    // Validate the data if necessary
-    // const parsedData = formSchema.parse(body);
     const { id, Amount, Remarks } = body
+
     await createDues({
       member_id: id,
       amount: parseFloat(Amount),
@@ -12,8 +11,7 @@ export async function POST(request: Request) {
       dues_type: 'dues',
       remarks: Remarks,
     } as Dues)
-    // Here you would typically process the data, e.g., save it to a database
-    // For demonstration, we will just return the received data
+
     return new Response(JSON.stringify({ success: true, data: body }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
