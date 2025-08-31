f5368c4ff01da48fa65dd022e7c0204f17592cc3
Raman Prasad <ramanprasad.0203@gmail.com>
2025-08-31 15:51:57 +0530

    fix: dotEnv.config issue

--- next.config.ts
+++ next.config.ts
@@ -4,36 +4,32 @@ import {
   PHASE_PRODUCTION_BUILD,
   PHASE_PRODUCTION_SERVER,
 } from 'next/constants'
-import dotenv from 'dotenv'
 
-function loadEnvFile(env: string) {
-  dotenv.config({ path: `.env.${env}` })
-}
+import { loadEnvConfig } from '@next/env'
+
+// function loadEnvFile(env: string) {
+//   dotenv.config({ path: `.env.${env}` })
+// }
 
 const nextConfig = (phase: string): NextConfig => {
-
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    loadEnvFile('development')
+    loadEnvConfig('.env.development')
   }
 
   if (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_PRODUCTION_SERVER) {
-    loadEnvFile('production')
-
-    // if (process.env.ENV === 'staging') {
-    //   loadEnvFile('staging')
-    // }
+    loadEnvConfig('.env.production')
   }
 
   return {
     reactStrictMode: true,
     serverExternalPackages: ['knex'],
-    env: {
-      NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
-      CONNECTION_STRING: process.env.CONNECTION_STRING,
-      // NEXT_PUBLIC_BASE_URL: process.env.host,
-      // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-      // NEXT_PUBLIC_LOG_LEVEL: process.env.NEXT_PUBLIC_LOG_LEVEL,
-    },
+    // env: {
+    //   NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
+    //   CONNECTION_STRING: process.env.CONNECTION_STRING,
+    //   // NEXT_PUBLIC_BASE_URL: process.env.host,
+    //   // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    //   // NEXT_PUBLIC_LOG_LEVEL: process.env.NEXT_PUBLIC_LOG_LEVEL,
+    // },
   }
 }
 
